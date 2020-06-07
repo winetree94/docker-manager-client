7ec47648a1208c6a537187d1bdd08bc0b9ccaadd
winetree94 <winetree94@outlook.com>
2020-06-07 20:41:38 +0900

    20200607

--- src/components/function_component_example.tsx
+++ src/components/function_component_example.tsx
@@ -4,6 +4,7 @@ export type IPerson = {
   name: string;
   age: number;
   click?(e: MouseEvent, name: string, age: number): void;
+  onRemoveClicked?(e: MouseEvent, name: string, age: number): void;
 };
 
 export const Person: FunctionComponent<IPerson> = (props) => (
@@ -11,6 +12,7 @@ export const Person: FunctionComponent<IPerson> = (props) => (
     <p onClick={(e: MouseEvent) => (props.click ? props.click(e, props.name, props.age) : undefined)}>
       name : {props.name} / age: {props.age}
     </p>
+    <input type="button" value="remove" onClick={(e: MouseEvent) => (props.onRemoveClicked ? props.onRemoveClicked(e, props.name, props.age) : undefined)} />
   </div>
 );
 
@@ -31,12 +33,35 @@ export const PersonManager: FunctionComponent<IPersonManager> = (props: IPersonM
 
   const onAddPersonClicked = (e: MouseEvent, name: string, age: string) => {
     e.preventDefault();
+
+    const existPerson = persons.find((person) => person.name === name);
+
+    if (!name) {
+      window.alert('please type name');
+      return;
+    }
+
+    if (existPerson) {
+      window.alert('duplicated');
+      return;
+    }
+
+    if (!parseInt(age)) {
+      window.alert('please type valid age ');
+      return;
+    }
+
     const person: IPerson = {
       name: name,
       age: parseInt(age),
       click: onPersonClicked,
     };
     setPersons([...persons, person]);
+  };
+
+  const onRemovePersonClicked = (e: MouseEvent, name: string, age: number) => {
+    e.preventDefault();
+    setPersons(persons.filter((person) => person.name !== name));
   };
 
   return (
@@ -48,7 +73,7 @@ export const PersonManager: FunctionComponent<IPersonManager> = (props: IPersonM
         <input type="button" value="사람 등록" onClick={(e) => onAddPersonClicked(e, name, age)} />
       </div>
       {persons.map((person) => (
-        <Person name={person.name} age={person.age} click={onPersonClicked} />
+        <Person key={person.name} name={person.name} age={person.age} click={onPersonClicked} onRemoveClicked={onRemovePersonClicked} />
       ))}
     </div>
   );
